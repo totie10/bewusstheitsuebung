a60eee87ce4a5516d053b962d85e20e826a1bc74
totie10 <tobias.tietz10@gmail.com>
2025-09-08 22:01:16 +0200

    first chat with gemini

---
+++ README.md
@@ -0,0 +1,10 @@
+## Setup free-tier google API key
+
+### 1. Create a free Google AI Studio account
+1. Go to [Google AI Studio](https://aistudio.google.com).
+2. Sign in with your Google account (free).
+3. Navigate to [API Keys](https://aistudio.google.com/app/apikey) → **Create API key**.
+4. Copy the key shown.
+
+### 2. Store your API key locally (do not commit it!)
+Set an environment variable called `GOOGLE_API_KEY`.
--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from google import genai
+import os
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f"Hi, {name}")  # Press Strg+F8 to toggle the breakpoint.
+def main():
+    client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"])
+    resp = client.models.generate_content(
+        model="gemini-2.0-flash-lite-001", contents="Is fear a feeling or a thought?."
+    )
+    print(resp.text)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    print_hi("PyCharm")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
